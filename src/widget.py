d7f2f5cd9f02dcbff9d86c298cc24da0a3df71d8
Андрей Стюхин <andreyst@olovyannaya.ru>
2025-02-14 12:13:05 +0900

    отредактированы докстринги get_date

--- src/widget.py
+++ src/widget.py
@@ -6,7 +6,10 @@ def mask_account_card(account_card: str) -> str | None:
     """Функция mask_account_card принимает на вход строку формата
     Visa Platinum 7000792289606361, или Maestro 7000792289606361, или Счет 73654108430135874305.
     Для маскировки номера карты/счета используются ранее написанные функции из модуля masks
+    :param account_card: Строка формата Visa Platinum 7000792289606361, или Maestro 7000792289606361, или Счет 73654108430135874305
+    :return: Строка формата Visa Platinum 7000 79** **** 6361, или Maestro 7000 79** **** 6361, или Счет **4305
     """
+
     if not account_card or not isinstance(account_card, str):
         return None
     try:
@@ -27,6 +30,10 @@ def mask_account_card(account_card: str) -> str | None:
 def get_date(date: str) -> str | None:
     """Функция get_date, которая принимает на вход строку с датой в формате
     "2024-03-11T02:26:18.671407" и возвращает строку с датой в формате
-    "ДД.ММ.ГГГГ" ("11.03.2024")'"""
+    "ДД.ММ.ГГГГ" ("11.03.2024")'
+    :param date: Строка с датой в формате "2024-03-11T02:26:18.671407"
+    :return: Строка с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")
+    """
+
     new_date: list[str] = date[0:10].split("-")
     return ".".join(new_date[::-1])
