939949bf648f8cf46676ab45419d9ef7f9b26ae2
Андрей Стюхин <andreyst@olovyannaya.ru>
2025-02-14 09:22:38 +0900

    в функцию mask_account_card добавлена читаемость

--- src/widget.py
+++ src/widget.py
@@ -3,21 +3,21 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str | None:
-    if not account_card or not isinstance(account_card, str):
-        return None
     """Функция mask_account_card принимает на вход строку формата
     Visa Platinum 7000792289606361, или Maestro 7000792289606361, или Счет 73654108430135874305.
     Для маскировки номера карты/счета используются ранее написанные функции из модуля masks
     """
+    if not account_card or not isinstance(account_card, str):
+        return None
     try:
-        number = account_card.split()[-1]
-        prefix = account_card[:-len(number)]
+        number = int(account_card.split()[-1])  # Преобразуем в целое число для проверки длины
+        prefix = account_card[:-len(str(number))]  # Преобразуем в строку для проверки длины
 
-        if len(number) == 16:
-            masked = get_mask_card_number(number)
+        if len(str(number)) == 16:
+            masked = get_mask_card_number(str(number))  # Преобразуем в строку для маскировки
             return f"{prefix}{masked}"
-        elif len(number) == 20:
-            masked = get_mask_account(number)
+        elif len(str(number)) == 20:
+            masked = get_mask_account(str(number))  # Преобразуем в строку для маскировки
             return f"{prefix}{masked}"
         return None
     except Exception:
